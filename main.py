e0e6d5dcb3b27b9bb873abaaa4861e6194e44f90
Kosta Zabashta <Kosta Zabashta>
2017-04-08 20:24:58 -0400

    Continue on tangerine parser

---
+++ main.py
@@ -0,0 +1,4 @@
+from parsers.tangerine import TangerineParser
+
+tg = TangerineParser('chequing')
+tg.parse_file('C:/Users/kosta/Documents/dev/finance/statements/tangerine/chequing/big_bang.csv')
--- parsers/generic_parser.py
+++ parsers/generic_parser.py
@@ -8,4 +8,6 @@ class GenericParser:
     __metaclass__ = ABCMeta
 
     @abstractmethod
-    def parse_file(self): pass
+    def parse_file(self):
+        """Parse a file and return a new instance of Transaction"""
+        pass
