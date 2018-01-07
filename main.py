9f28f944718b16dd5094c82236aff8c08c79807a
kzabashta <kosta.zabashta@gmail.com>
2018-01-07 13:53:52 -0500

    Working on combined report

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import matplotlib.pylab as plt
 
 from initializer import config_reader
 
-def save_combined_figure(df):
+def save_combined_figure(accounts):
+        """
         x = df['tx_date']
         y = df['cum_sum']
 
@@ -16,6 +17,17 @@ def save_combined_figure(df):
         plt.ylabel('Holdings')
         fig.savefig("plots/combined.png")   # save the figure to file
         plt.close(fig) 
+        """
+        fig = plt.figure()
+        fig.subplots_adjust(hspace=0.4, wspace=0.4)
+        for i in range(1, len(accounts)):
+            ax = fig.add_subplot(len(accounts) / 2, 2, i)
+
+            x = accounts[i].transactions['tx_date']
+            y = accounts[i].transactions['cum_sum']
+            ax.plot(x, y)
+        fig.set_size_inches(18.5, 18.5)
+        fig.savefig("plots/combined.png", dpi=100)
 
 if __name__ == '__main__':
     config_reader = config_reader.ConfigReader('./statements/config.json')
@@ -32,4 +44,4 @@ if __name__ == '__main__':
     combined_tx = combined_tx.sort_values('tx_date')
     combined_tx = combined_tx.set_index(pd.DatetimeIndex(combined_tx['tx_date']))
     combined_tx['cum_sum'] = combined_tx.tx_amount.cumsum()
-    save_combined_figure(combined_tx)
+    save_combined_figure(accounts)
