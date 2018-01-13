6acf8bf9b49782ee86103cc78ff0f2c2cbc9f6b6
kzabashta <kosta.zabashta@gmail.com>
2018-01-13 13:30:23 -0500

    Continue on grid search for ARIMA

--- main.py
+++ main.py
@@ -50,6 +50,7 @@ def save_interpolated_figure(df):
     ts = ts.resample('D').mean()
 
     ts = ts.interpolate(method='spline', order=3)
+    arima_predict(ts)
     
     fig, ax = plt.subplots(nrows=1, ncols=1)
     ax.plot(ts)
@@ -59,6 +60,40 @@ def save_interpolated_figure(df):
     fig.set_size_inches(18.5, 18.5)
     fig.savefig("plots/interpolated.png", dpi=100)
     plt.close(fig)
+
+def arima_predict(ts):
+    p = d = q = range(0, 2)
+    pdq = list(itertools.product(p, d, q))
+    seasonal_pdq = [(x[0], x[1], x[2], 12) for x in list(itertools.product(p, d, q))]
+    lowest_pdq = lowest_seasonal_pdq = lowest_aic = None
+    for param in pdq:
+        for param_seasonal in seasonal_pdq:
+            try:
+                mod = sm.tsa.statespace.SARIMAX(ts,
+                                                order=param,
+                                                seasonal_order=param_seasonal,
+                                                enforce_stationarity=False,
+                                                enforce_invertibility=False)
+
+                results = mod.fit()
+                if lowest_aic is None:
+                    lowest_aic = results.aic
+                elif results.aic < lowest_aic:
+                    lowest_pdq = pdq
+                    lowest_seasonal_pdq = seasonal_pdq
+                    lowest_aic = lowest_aic
+            except:
+                continue
+    
+    mod = sm.tsa.statespace.SARIMAX(y,
+                                order=lowest_pdq,
+                                seasonal_order=lowest_seasonal_pdq,
+                                enforce_stationarity=False,
+                                enforce_invertibility=False)
+
+    results = mod.fit()
+
+    print(results.summary().tables[1])
 
 if __name__ == '__main__':
     config_reader = config_reader.ConfigReader('./statements/config.json')
