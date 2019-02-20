72a542b7a2425ac8b42dc8bccce4286f5198411f
kzabashta <kosta.zabashta@gmail.com>
2019-02-19 22:45:36 -0500

    Refactoring

--- entities/account.py
+++ entities/account.py
@@ -24,13 +24,14 @@ class Account():
             else:
                 return row['amount_credit']
 
-        dfs = []
-        filenames = glob.glob(self.tx_fpath + "/*.csv")
+        transaction_dfs = []
+        transaction_filenames = glob.glob(self.tx_fpath + "/*.csv")
 
-        for filename in filenames:
-            logging.debug('Processing file %s' % filename)
+        for transaction_filename in transaction_filenames:
+            logging.debug('Processing file %s' % transaction_filename)
+
             skiprows = 1 if self.is_header else None
-            df = pd.read_csv(filename, names=self.file_cols, index_col=False, skiprows=skiprows, header=None)
+            df = pd.read_csv(transaction_filename, names=self.file_cols, index_col=False, skiprows=skiprows, header=None)
             if self.amount_split:
                 df['amount'] = df.apply(get_amount, axis=1)
             df = df.rename(columns=self.alias_cols)
@@ -38,13 +39,11 @@ class Account():
             df['tx_amount'] *= self.ratio
             df['account'] = self.name
             df['hash'] = df.apply(lambda x: hash(tuple(x)), axis = 1)
-            dfs.append(df)
+            transaction_dfs.append(df)
 
-        return pd.concat(dfs)
+        return pd.concat(transaction_dfs)
 
     def __init_transactions(self):
-
-
         # Concatenate all data into one DataFrame
         transactions = self.__read_transactions()
         transactions['tx_date'] = pd.to_datetime(transactions['tx_date'])
